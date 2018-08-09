bea022251867f0e9ddf1c20855d5f6db995e2e90
Ryoichi Gokan <nargok@gmail.com>
2018-08-10 08:03:17 +0900

    link to next page

--- hello/views.py
+++ hello/views.py
@@ -3,8 +3,18 @@ from django.http import HttpResponse
 
 def index(request):
   params = {
-      'title':'Hello/Index',
-      'msg':'これは、サンプルで作ったページです',
+    'title':'Hello/Index',
+    'msg':'これは、サンプルで作ったページです',
+    'goto':'next',
+  }
+
+  return render(request, 'hello/index.html', params)
+
+def next(request):
+  params = {
+    'title':'Hello/Next',
+    'msg':'これは、もう一つのページです',
+    'goto':'index',
   }
 
   return render(request, 'hello/index.html', params)
