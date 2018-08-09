80e4624d25d2ba1c1a910416bba4820dc334fef1
Ryoichi Gokan <nargok@gmail.com>
2018-08-10 08:36:28 +0900

    input message in form tag

--- hello/views.py
+++ hello/views.py
@@ -4,17 +4,16 @@ from django.http import HttpResponse
 def index(request):
   params = {
     'title':'Hello/Index',
-    'msg':'これは、サンプルで作ったページです',
-    'goto':'next',
+    'msg':'お名前は？',
   }
 
   return render(request, 'hello/index.html', params)
 
-def next(request):
+def form(request):
+  msg = request.POST['msg']
   params = {
-    'title':'Hello/Next',
-    'msg':'これは、もう一つのページです',
-    'goto':'index',
+    'title':'Hello/Form',
+    'msg':'こんにちは、' + msg + 'さん',
   }
 
   return render(request, 'hello/index.html', params)
