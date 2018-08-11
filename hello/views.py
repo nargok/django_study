9828ab85e4a4e4d32c2cb76da4804f0f50c8dde6
Ryoichi Gokan <nargok@gmail.com>
2018-08-11 12:39:23 +0900

    customize __str__ function

--- hello/views.py
+++ hello/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import  render
 from django.http import HttpResponse
 from .models import Friend
-from .forms import HelloForm
+from django.db.models import QuerySet
+
+def __new_str__(self):
+  result = ''
+  for item in self:
+    result += '<tr>'
+    for k in item:
+      result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+    result += '</tr>'
+  return result
+
+# 任意の表示内容にできるよう__str__関数をカスタマイズする
+QuerySet.__str__=__new_str__
 
 def index(request):
+  data = Friend.objects.values('id', 'name', 'age')
   params = {
     'title': 'Hello',
-    'message': 'all friends',
-    'form': HelloForm(),
-    'data': [],
+    'data': data,
   }
-  if (request.method == 'POST'):
-    num = request.POST['id']
-    item = Friend.objects.get(id=num)
-    params['data'] = [item]
-    params['form'] = HelloForm(request.POST)
-  else:
-    params['data'] = Friend.objects.all()
 
   return render(request, 'hello/index.html', params)
