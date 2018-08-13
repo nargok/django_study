3558138be51e8541b1a11b57d9a7ce4b2f98d664
Ryoichi Gokan <nargok@gmail.com>
2018-08-13 13:59:36 +0900

    add view function

--- sns/views.py
+++ sns/views.py
@@ -135,4 +135,36 @@ def groups(request):
 
   return render(request, 'sns/groups.html', params)
 
+# Friendの追加処理
+@login_required(login_url='/admin/login/')
+def add(request):
+  # 追加するUserを取得
+  add_name = request.GET['name']
+  add_user = User.objects.filter(username=add_name).first()
+  # Userが本人だった場合の処理
+  if add_user == request.user:
+    messages.info(request, "自分自身をFriendに追加することはできません")
+    return redirect(to='/sns')
+  # publicの取得
+  (public_user, public_group) = get_public()
+  # add_userのFriendの数を調べる
+  frd_num = Friend.objects.filter(owner=request.user) \
+            .filter(user=add_user).count()
+  # ゼロより大きければ既に登録済
+  if frd_num > 0:
+    messages.info(request, add_user.username + \
+                  'は既に追加されています')
+    return  redirect(to='/sns')
 
+  # ここからFriendの登録処理
+  frd = Friend()
+  frd.owner = request.user
+  frd.user = add_user
+  frd.group = public_group
+  frd.save()
+  # メッセージを設定
+  messages.success(request,
+                   add_user.username +
+                   'を設定しました! groupページに移動して、\
+                   追加したFriendをメンバーに設定してください')
+  return redirect(to='/sns')
