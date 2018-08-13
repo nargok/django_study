942508664bdd3b18cebf75155b441a68bbb97ef8
Ryoichi Gokan <nargok@gmail.com>
2018-08-13 15:50:20 +0900

    GroupCheckFormのバグを解消

--- sns/forms.py
+++ sns/forms.py
@@ -36,8 +36,9 @@ class GroupCheckForm(forms.Form):
   def __init__(self, user, *args, **kwargs):
     super(GroupCheckForm, self).__init__(*args, **kwargs)
     public = User.objects.filter(username='public').first()
-    self.fields['group'] = forms.MultipleChoiceField(
-      choices=[(item.title, item.title)] for item in Group.objects.filter(owner__in=[user,public]),
+    self.fields['groups'] = forms.MultipleChoiceField(
+      choices=[(item.title, item.title) for item in \
+        Group.objects.filter(owner__in=[user, public])],
       widget=forms.CheckboxSelectMultiple(),
     )
 
